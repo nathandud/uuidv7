678832a05774bade48e2f59a800802aafc540e6a
Nathan Dudley <dudleynathan@byu.net>
2024-08-16 21:07:01 +1200

    Simplify Swift uuid7 function

--- src/uuidv7.swift
+++ src/uuidv7.swift
@@ -2,17 +2,19 @@ import Foundation
 
 extension UUID {
     static func v7() -> Self {
-        // random bytes
-        var value = (
-            UInt8(0),
-            UInt8(0),
-            UInt8(0),
-            UInt8(0),
-            UInt8(0),
-            UInt8(0),
+        // current timestamp in ms
+        let timestamp: Int = .init(Date().timeIntervalSince1970 * 1000)
+        
+        let uuid: uuid_t = (
+            UInt8((timestamp >> 40) & 0xFF),
+            UInt8((timestamp >> 32) & 0xFF),
+            UInt8((timestamp >> 24) & 0xFF),
+            UInt8((timestamp >> 16) & 0xFF),
+            UInt8((timestamp >> 8) & 0xFF),
+            UInt8((timestamp >> 0) & 0xFF),
+            UInt8.random(in: 0...255) & 0x0F | 0x70, // version
             UInt8.random(in: 0...255),
-            UInt8.random(in: 0...255),
-            UInt8.random(in: 0...255),
+            UInt8.random(in: 0...255) & 0x3F | 0x80, // variant
             UInt8.random(in: 0...255),
             UInt8.random(in: 0...255),
             UInt8.random(in: 0...255),
@@ -21,23 +23,8 @@ extension UUID {
             UInt8.random(in: 0...255),
             UInt8.random(in: 0...255)
         )
-
-        // current timestamp in ms
-        let timestamp: Int = .init(Date().timeIntervalSince1970 * 1000)
-
-        // timestamp
-        value.0 = .init((timestamp >> 40) & 0xFF)
-        value.1 = .init((timestamp >> 32) & 0xFF)
-        value.2 = .init((timestamp >> 24) & 0xFF)
-        value.3 = .init((timestamp >> 16) & 0xFF)
-        value.4 = .init((timestamp >> 8) & 0xFF)
-        value.5 = .init(timestamp & 0xFF)
-
-        // version and variant
-        value.6 = (value.6 & 0x0F) | 0x70
-        value.8 = (value.8 & 0x3F) | 0x80
-
-        return UUID(uuid: value)
+        
+        return UUID(uuid: uuid)
     }
 }
 
